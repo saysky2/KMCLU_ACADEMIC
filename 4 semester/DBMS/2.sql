df5edd92f0c15502f6964e777ce68687d38e71a9
saysky2 <10852akash@gmail.com>
2024-04-02 00:36:17 +0530

    logical operators

--- 4 semester/DBMS/2.sql
+++ 4 semester/DBMS/2.sql
@@ -1,6 +1,25 @@
 -- Write SQL queries using logical operator 
 
-CREATE DATABASE logicalop;
+CREATE DATABASE IF NOT EXISTS LOGICAL_OPERATORS;
 
-USE logicalop;
+USE LOGICAL_OPERATORS;
 
+ CREATE TABLE IF NOT EXISTS KMCLU_BTECH(
+    RollNo INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Email VARCHAR(50)
+);
+
+INSERT INTO KMCLU_BTECH VALUES 
+(8, "Akash", "Gupta", "AI&ML", "xyz@gmail.com"), 
+(28, "Vaibhav", "Yadav", "CSE", "abc@gmail.com"), 
+(51, "Alifarm", "Khan", "CIVIL", "pqs@gmail.com");
+
+SELECT * FROM KMCLU_BTECH; -- showing all the data 
+
+-- AND Operator
+SELECT * FROM KMCLU_BTECH WHERE FirstNAME = 'ALIFARM' AND Department = 'CIVIL';
+
+-- IR Operator
